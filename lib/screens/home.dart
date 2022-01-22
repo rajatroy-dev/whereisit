e5031fd826f3f33775470ba578e735d1ace3133c
Rajat Roy <rajatroy.dev@gmail.com>
2022-01-22 22:58:17 +0530

    Added Home Screen elements

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,8 +5,93 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Screen'),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 100,
+                width: 150,
+                child: Card(
+                  elevation: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.apartment_rounded,
+                        size: 40,
+                      ),
+                      Text('Property'),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                width: 150,
+                child: Card(
+                  elevation: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.space_dashboard_outlined,
+                        size: 40,
+                      ),
+                      Text('Room'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 100,
+                width: 150,
+                child: Card(
+                  elevation: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.all_inbox_rounded,
+                        size: 40,
+                      ),
+                      Text('Area'),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                width: 150,
+                child: Card(
+                  elevation: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.move_to_inbox_rounded,
+                        size: 40,
+                      ),
+                      Text('Item'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ test/screens/home_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:whereisit/screens/home.dart';
+
+void main() {
+  testWidgets('renders home screen', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: HomeScreen(),
+        ),
+      ),
+    );
+
+    expect(find.text('Property'), findsOneWidget);
+    expect(find.byIcon(Icons.apartment_rounded), findsOneWidget);
+
+    expect(find.text('Room'), findsOneWidget);
+    expect(find.byIcon(Icons.space_dashboard_outlined), findsOneWidget);
+
+    expect(find.text('Area'), findsOneWidget);
+    expect(find.byIcon(Icons.all_inbox_rounded), findsOneWidget);
+
+    expect(find.text('Item'), findsOneWidget);
+    expect(find.byIcon(Icons.move_to_inbox_rounded), findsOneWidget);
+  });
+}
