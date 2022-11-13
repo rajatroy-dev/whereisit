599ea7f84d494cb60b5368edd35db33046153724
Rajat Roy <rajatroy.dev@gmail.com>
2022-11-13 20:02:31 +0530

    Change in DB schema

--- lib/db/db_provider.dart
+++ lib/db/db_provider.dart
@@ -38,74 +38,130 @@ class DBProvider {
   }
 
   static Future<void> _onCreate(Database db, int version) async {
+    // users
+    db.execute('CREATE TABLE users ('
+        'id INTEGER PRIMARY KEY, '
+        'name TEXT NOT NULL, '
+        'username TEXT NOT NULL UNIQUE, '
+        'created_by TEXT NOT NULL, '
+        'created_at TEXT NOT NULL, '
+        'updated_by TEXT NOT NULL, '
+        'updated_at TEXT NOT NULL'
+        ');');
+
+    // location is like a geographical location
+    // It will have an address
+    // It can also have co-ordinates
     await db.execute('CREATE TABLE locations ('
         'id INTEGER PRIMARY KEY, '
-        'location TEXT, '
-        'address TEXT NOT NULL UNIQUE'
-        ')');
+        'latitude TEXT, '
+        'longitude TEXT, '
+        'address TEXT NOT NULL UNIQUE, '
+        'created_by TEXT NOT NULL, '
+        'created_at TEXT NOT NULL, '
+        'updated_by TEXT NOT NULL, '
+        'updated_at TEXT NOT NULL'
+        ');');
 
+    // A house property could be your home, an office, a shop, a building or
+    // some land attached to the building like a parking lot.
     await db.execute('CREATE TABLE properties ('
         'id INTEGER PRIMARY KEY, '
-        'location_id INTEGER, '
+        'location_id INTEGER REFERENCES locations(id) ON DELETE RESTRICT, '
         'name TEXT NOT NULL UNIQUE, '
-        'FOREIGN KEY (location_id) REFERENCES locations (id) ON DELETE RESTRICT'
-        ')');
+        'created_by TEXT NOT NULL, '
+        'created_at TEXT NOT NULL, '
+        'updated_by TEXT NOT NULL, '
+        'updated_at TEXT NOT NULL'
+        ');');
 
+    // Room exists in a property
     await db.execute('CREATE TABLE rooms ('
         'id INTEGER PRIMARY KEY, '
-        'name TEXT NOT NULL UNIQUE'
-        ')');
+        'name TEXT NOT NULL, '
+        'created_by TEXT NOT NULL, '
+        'created_at TEXT NOT NULL, '
+        'updated_by TEXT NOT NULL, '
+        'updated_at TEXT NOT NULL'
+        ');');
 
+    // The items stored will normally be categorized like books, electronics ...
     await db.execute('CREATE TABLE categories ('
         'id INTEGER PRIMARY KEY, '
-        'name TEXT NOT NULL UNIQUE'
-        ')');
+        'name TEXT NOT NULL UNIQUE, '
+        'created_by TEXT NOT NULL, '
+        'created_at TEXT NOT NULL, '
+        'updated_by TEXT NOT NULL, '
+        'updated_at TEXT NOT NULL'
+        ');');
 
+    DateTime now = DateTime.now();
+    String isoDate = now.toIso8601String();
     await db.execute('INSERT INTO categories '
-        '(name) '
+        '(name, '
+        'created_by, '
+        'created_at, '
+        'updated_by, '
+        'updated_at) '
         'VALUES '
-        '(\'Uncategorized\')');
+        '(\'Uncategorized\', '
+        'system, '
+        '$isoDate, '
+        'system, '
+        '$isoDate);');
 
+    // Items are the things that are stored
     await db.execute('CREATE TABLE items ('
         'id INTEGER PRIMARY KEY, '
-        'property_id INTEGER, '
-        'room_id INTEGER, '
-        'category_id INTEGER DEFAULT 1 NOT NULL, '
+        'location_id INTEGER REFERENCES locations(id) ON DELETE RESTRICT, '
+        'property_id INTEGER REFERENCES properties(id) ON DELETE RESTRICT, '
+        'room_id INTEGER REFERENCES rooms(id) ON DELETE RESTRICT, '
+        'category_id INTEGER DEFAULT 1 NOT NULL REFERENCES categories(id) ON DELETE RESTRICT, '
         'name TEXT NOT NULL, '
         'thumbnail TEXT NOT NULL, '
         'quantity INTEGER DEFAULT 1 NOT NULL, '
         'favorite INTEGER DEFAULT 0 NOT NULL CHECK (favorite IN (0, 1)), '
-        'location_id INTEGER, '
         'serial TEXT, '
         'description TEXT, '
         'qr TEXT UNIQUE, '
-        'createdAt INTEGER NOT NULL, '
-        'updatedAt INTEGER NOT NULL, '
-        'FOREIGN KEY (property_id) REFERENCES properties (id) ON DELETE RESTRICT, '
-        'FOREIGN KEY (room_id) REFERENCES rooms (id) ON DELETE RESTRICT, '
-        'FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE RESTRICT, '
-        'FOREIGN KEY (location_id) REFERENCES locations (id) ON DELETE RESTRICT'
-        ')');
+        'created_by TEXT NOT NULL, '
+        'created_at TEXT NOT NULL, '
+        'updated_by TEXT NOT NULL, '
+        'updated_at TEXT NOT NULL'
+        ');');
 
-    await db.execute('CREATE TABLE images ('
+    // Apart from thumbnail, an item can have multiple images
+    db.execute('CREATE TABLE images ('
         'id INTEGER PRIMARY KEY, '
-        'item_id INTEGER NOT NULL, '
-        'filename TEXT NOT NULL UNIQUE, '
-        'filelocation TEXT NOT NULL UNIQUE, '
-        'FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE CASCADE'
-        ')');
+        // items are the main entity
+        // so, deleting an item will have cascading effect on all other entities
+        'item_id INTEGER NOT NULL REFERENCES items(id) ON DELETE CASCADE, '
+        'filename TEXT NOT NULL, '
+        'file_location TEXT NOT NULL UNIQUE, '
+        'created_by TEXT NOT NULL, '
+        'created_at TEXT NOT NULL, '
+        'updated_by TEXT NOT NULL, '
+        'updated_at TEXT NOT NULL'
+        ');');
 
+    // Tags are another way of categorizing items
     await db.execute('CREATE TABLE tags ('
         'id INTEGER PRIMARY KEY, '
-        'name TEXT NOT NULL UNIQUE'
-        ')');
+        'name TEXT NOT NULL UNIQUE, '
+        'created_by TEXT NOT NULL, '
+        'created_at TEXT NOT NULL, '
+        'updated_by TEXT NOT NULL, '
+        'updated_at TEXT NOT NULL'
+        ');');
 
+    // An item can have multiple tags
+    // A tag can be used by multiple items
     await db.execute('CREATE TABLE items_tags ('
-        'id INTEGER PRIMARY KEY, '
-        'item_id INTEGER, '
-        'tag_id INTEGER NOT NULL, '
-        'FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE SET NULL, '
-        'FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE'
-        ')');
+        'PRIMARY KEY (item_id, tag_id), '
+        // items are the main entity
+        // so, deleting an item will have cascading effect on all other entities
+        'item_id INTEGER REFERENCES items(id) ON DELETE CASCADE, '
+        'tag_id INTEGER NOT NULL REFERENCES tags(id) ON DELETE RESTRICT'
+        ');');
   }
 }
