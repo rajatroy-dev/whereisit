7cdd28b1b8bf77c349e83708384dab41a94e90e9
Rajat Roy <rajatroy.dev@gmail.com>
2023-02-22 08:09:47 +0530

    Refactor add images

--- lib/screens/add/add.screen.dart
+++ lib/screens/add/add.screen.dart
@@ -6,6 +6,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:whereisit/shared/enums/source_choice.enum.dart';
 import 'package:whereisit/shared/intents/route_arguments.intent.dart';
 import 'package:whereisit/shared/widgets/edit_item/edit_item.viewgroup.dart';
+import 'package:whereisit/shared/widgets/image_source_choice/image_source_choice_popup.dart';
 
 class Add extends StatefulWidget {
   const Add({Key? key}) : super(key: key);
@@ -23,31 +24,28 @@ class _AddState extends State<Add> {
     });
   }
 
+  sendImagetoEdit(XFile image) async {
+    Directory tempDir = await getApplicationDocumentsDirectory();
+    var path = '${tempDir.path}/${image.name}';
+    image.saveTo(path);
+    Navigator.pushNamed(
+      context,
+      EditItem.routeName,
+      arguments: RouteArguments(true, params: {'image': image.name}),
+    );
+  }
+
   handleImageSourceSelection(BuildContext context, SourceChoice choice) async {
     ImagePicker picker = ImagePicker();
 
     switch (choice) {
       case SourceChoice.gallery:
         var image = await picker.pickImage(source: ImageSource.gallery);
-        Directory tempDir = await getApplicationDocumentsDirectory();
-        var path = '${tempDir.path}/${image!.name}';
-        image.saveTo(path);
-        Navigator.pushNamed(
-          context,
-          EditItem.routeName,
-          arguments: RouteArguments(true, params: {'image': image.name}),
-        );
+        await sendImagetoEdit(image!);
         break;
       case SourceChoice.camera:
         var image = await picker.pickImage(source: ImageSource.camera);
-        Directory tempDir = await getApplicationDocumentsDirectory();
-        var path = '${tempDir.path}/${image!.name}';
-        image.saveTo(path);
-        Navigator.pushNamed(
-          context,
-          EditItem.routeName,
-          arguments: RouteArguments(true, params: {'image': image.name}),
-        );
+        await sendImagetoEdit(image!);
         break;
       case SourceChoice.none:
         setState(() {
@@ -61,32 +59,43 @@ class _AddState extends State<Add> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        height: 200,
-        child: Column(
-          children: [
-            ElevatedButton.icon(
-              onPressed: handleAddImage,
-              icon: const Icon(Icons.add_rounded),
-              label: const Text('Add Item'),
+    return Stack(
+      children: [
+        Center(
+          child: SizedBox(
+            height: 200,
+            child: Column(
+              children: [
+                ElevatedButton.icon(
+                  onPressed: handleAddImage,
+                  icon: const Icon(Icons.add_rounded),
+                  label: const Text('Add Item'),
+                ),
+                const Text(
+                  '— OR —',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black26,
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.luggage_rounded),
+                  label: const Text('Add Items'),
+                ),
+                const EditItem(),
+              ],
             ),
-            const Text(
-              '— OR —',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.black26,
-              ),
-            ),
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.luggage_rounded),
-              label: const Text('Add Items'),
-            ),
-            const EditItem(),
-          ],
+          ),
         ),
-      ),
+        if (showImageSourceChoice)
+          ImageSourceChoicePopup(
+            handler: (SourceChoice choice) => handleImageSourceSelection(
+              context,
+              choice,
+            ),
+          ),
+      ],
     );
   }
 }
--- lib/shared/widgets/horizontal_image_list_container/horizontal_image_list_container.viewgroup.dart
+++ lib/shared/widgets/horizontal_image_list_container/horizontal_image_list_container.viewgroup.dart
@@ -1,8 +1,56 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:whereisit/shared/enums/source_choice.enum.dart';
 import 'package:whereisit/shared/widgets/horizontal_image_list_container/horizontail_list_image/horizontal_list_image.viewgroup.dart';
 
-class HorizontalImageListContainer extends StatelessWidget {
+class HorizontalImageListContainer extends StatefulWidget {
   const HorizontalImageListContainer({Key? key}) : super(key: key);
+
+  @override
+  State<HorizontalImageListContainer> createState() =>
+      _HorizontalImageListContainerState();
+}
+
+class _HorizontalImageListContainerState
+    extends State<HorizontalImageListContainer> {
+  var showImageSourceChoice = false;
+
+  handleAddImage() {
+    setState(() {
+      showImageSourceChoice = true;
+    });
+  }
+
+  saveImage(XFile image) async {
+    Directory tempDir = await getApplicationDocumentsDirectory();
+    var path = '${tempDir.path}/${image.name}';
+    image.saveTo(path);
+  }
+
+  handleImageSourceSelection(BuildContext context, SourceChoice choice) async {
+    ImagePicker picker = ImagePicker();
+
+    switch (choice) {
+      case SourceChoice.gallery:
+        var image = await picker.pickImage(source: ImageSource.gallery);
+        await saveImage(image!);
+        break;
+      case SourceChoice.camera:
+        var image = await picker.pickImage(source: ImageSource.camera);
+        await saveImage(image!);
+        break;
+      case SourceChoice.none:
+        setState(() {
+          showImageSourceChoice = false;
+        });
+        return;
+      default:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,9 +68,9 @@ class HorizontalImageListContainer extends StatelessWidget {
           ),
         ),
         ElevatedButton.icon(
-          onPressed: () {},
+          onPressed: handleAddImage,
           icon: const Icon(Icons.add_rounded),
-          label: const Text('Add'),
+          label: const Text('Add more'),
         ),
       ],
     );
