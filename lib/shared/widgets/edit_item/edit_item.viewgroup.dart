6c92961260b33f78878f2f8517051d46752ac48a
Rajat Roy <rajatroy.dev@gmail.com>
2023-02-23 21:50:43 +0530

    Change display of horizontal list of images and edit item screen

--- lib/shared/widgets/edit_item/edit_item.viewgroup.dart
+++ lib/shared/widgets/edit_item/edit_item.viewgroup.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:whereisit/shared/bloc/update_item_bloc.dart';
+import 'package:whereisit/shared/enums/source_choice.enum.dart';
 import 'package:whereisit/shared/intents/route_arguments.intent.dart';
+import 'package:whereisit/shared/widgets/horizontal_image_list_container/horizontal_image_list_container.viewgroup.dart';
+import 'package:whereisit/shared/widgets/image_source_choice/image_source_choice_popup.dart';
 import 'package:whereisit/shared/widgets/pill_tag.view.dart';
 
 class EditItem extends StatefulWidget {
@@ -14,7 +21,44 @@ class EditItem extends StatefulWidget {
 }
 
 class _EditItemState extends State<EditItem> {
+  final _formKey = GlobalKey<FormState>();
+
   var imageName = '';
+  var showImageSourceChoice = false;
+
+  handleAddImage() {
+    setState(() {
+      showImageSourceChoice = true;
+    });
+  }
+
+  saveImage(XFile image) async {
+    Directory tempDir = await getApplicationDocumentsDirectory();
+    var path = '${tempDir.path}/${image.name}';
+    image.saveTo(path);
+  }
+
+  handleImageSourceSelection(SourceChoice choice) async {
+    ImagePicker picker = ImagePicker();
+
+    switch (choice) {
+      case SourceChoice.gallery:
+        var image = await picker.pickImage(source: ImageSource.gallery);
+        await saveImage(image!);
+        break;
+      case SourceChoice.camera:
+        var image = await picker.pickImage(source: ImageSource.camera);
+        await saveImage(image!);
+        break;
+      case SourceChoice.none:
+        setState(() {
+          showImageSourceChoice = false;
+        });
+        return;
+      default:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,31 +101,46 @@ class _EditItemState extends State<EditItem> {
               ),
             ],
           );
-        } else if (state is UpdateItemNewSuccess) {
-          return Wrap(
-            children: const [
-              PillTag(
-                title: '# This is a very large tag',
-                isShort: false,
-              ),
-              PillTag(
-                title: '# This is a large tag',
-                isShort: false,
-              ),
-              PillTag(
-                title: '# This is a large tag',
-                isShort: false,
-              ),
-              PillTag(
-                title: '# This is a large tag',
-                isShort: false,
-              ),
-            ],
-          );
         }
 
-        return const Center(
-          child: Text('Something Went Wrong!'),
+        return Form(
+          key: _formKey,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  HorizontalImageListContainer(
+                    addImage: handleAddImage,
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processing Data')),
+                          );
+                        }
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                ],
+              ),
+              if (showImageSourceChoice)
+                ImageSourceChoicePopup(
+                  handler: handleImageSourceSelection,
+                ),
+            ],
+          ),
         );
       },
     );
--- lib/shared/widgets/horizontal_image_list_container/horizontal_image_list_container.viewgroup.dart
+++ lib/shared/widgets/horizontal_image_list_container/horizontal_image_list_container.viewgroup.dart
@@ -1,56 +1,12 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:whereisit/shared/enums/source_choice.enum.dart';
 import 'package:whereisit/shared/widgets/horizontal_image_list_container/horizontail_list_image/horizontal_list_image.viewgroup.dart';
 
-class HorizontalImageListContainer extends StatefulWidget {
-  const HorizontalImageListContainer({Key? key}) : super(key: key);
-
-  @override
-  State<HorizontalImageListContainer> createState() =>
-      _HorizontalImageListContainerState();
-}
-
-class _HorizontalImageListContainerState
-    extends State<HorizontalImageListContainer> {
-  var showImageSourceChoice = false;
-
-  handleAddImage() {
-    setState(() {
-      showImageSourceChoice = true;
-    });
-  }
-
-  saveImage(XFile image) async {
-    Directory tempDir = await getApplicationDocumentsDirectory();
-    var path = '${tempDir.path}/${image.name}';
-    image.saveTo(path);
-  }
-
-  handleImageSourceSelection(BuildContext context, SourceChoice choice) async {
-    ImagePicker picker = ImagePicker();
-
-    switch (choice) {
-      case SourceChoice.gallery:
-        var image = await picker.pickImage(source: ImageSource.gallery);
-        await saveImage(image!);
-        break;
-      case SourceChoice.camera:
-        var image = await picker.pickImage(source: ImageSource.camera);
-        await saveImage(image!);
-        break;
-      case SourceChoice.none:
-        setState(() {
-          showImageSourceChoice = false;
-        });
-        return;
-      default:
-        break;
-    }
-  }
+class HorizontalImageListContainer extends StatelessWidget {
+  final void Function() addImage;
+  const HorizontalImageListContainer({
+    Key? key,
+    required this.addImage,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +24,7 @@ class _HorizontalImageListContainerState
           ),
         ),
         ElevatedButton.icon(
-          onPressed: handleAddImage,
+          onPressed: addImage,
           icon: const Icon(Icons.add_rounded),
           label: const Text('Add more'),
         ),
