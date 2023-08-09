85b6124da5c5d29d0a735aefecafbebb9958bae4
Rajat Roy <rajatroy.dev@gmail.com>
2023-08-09 21:04:14 +0530

    Refactor

--- lib/data/repository/property_repository.dart
+++ lib/data/repository/property_repository.dart
@@ -5,7 +5,8 @@ class PropertyRepository {
   final dao = PropertyDao();
 
   Future<int> insert(Property property) => dao.insert(property);
-  Future<Property> findById(int id) => dao.findById(id);
+  Future<Property?> findById(int id) => dao.findById(id);
+  Future<int> getTotalProperties() => dao.getTotalProperties();
   Future<List<Property>> findAll() => dao.findAll();
   Future<int> update(Property property) => dao.update(property);
   Future<int> delete(int id) => dao.delete(id);
