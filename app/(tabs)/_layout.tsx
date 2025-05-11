19de63fe18739ad114e38a16f8d922d86b8312f2
Rajat Roy <rajatroy.dev@gmail.com>
2025-05-12 02:07:33 +0530

    Add Tamagui Library

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import IconSymbol from '@/components/ui/IconSymbol';
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: 'absolute',
+          },
+          default: {},
+        }),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="home" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
