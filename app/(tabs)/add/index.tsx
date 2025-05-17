3273369551aac443d1502837768d35bf55467462
Rajat Roy <rajatroy.dev@gmail.com>
2025-05-17 13:13:34 +0530

    WIP Add Screen

--- app/(tabs)/add/index.tsx
+++ app/(tabs)/add/index.tsx
@@ -1,47 +1,46 @@
 import CustomSafeAreaView from "@/components/ui/CustomSafeAreaView";
+import IconSymbol from "@/components/ui/IconSymbol";
 import useAppStore from "@/state/app-store";
-import { Text } from "@tamagui/core";
 import { useMemo, useState } from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function AddPage() {
-    const setTheme = useAppStore(state => state.setTheme);
     const theme = useAppStore(state => state.theme);
 
-    const [currentTheme, setCurrentTheme] = useState('dark');
-    const [counter, setCounter] = useState(1);
+    const [isItemFocused, setItemFocused] = useState(false);
 
     const styles = useMemo(() => stylesheet(
         theme.color.val,
         theme.accentColor.val,
-        theme.background.val
+        theme.background.val,
+        theme.borderColor.val,
+        theme.borderColorFocus.val,
+        theme.shadowColor.val
     ), [theme]);
-
-    const handleTheme = () => {
-        if (currentTheme === 'dark') {
-            setTheme('light');
-            setCurrentTheme('light');
-        } else {
-            setTheme('dark');
-            setCurrentTheme('dark');
-        }
-    }
 
     return (
         <CustomSafeAreaView style={styles.screen}>
             <View style={styles.container}>
-                <Text style={styles.text}>Where Is It?</Text>
-                <Text style={styles.text}>{counter}</Text>
-                <TouchableOpacity
-                    onPress={handleTheme}
-                    style={styles.button}>
-                    <Text style={styles.text}>Toggle Theme</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => setCounter(counter + 1)}
-                    style={styles.button}>
-                    <Text style={styles.text}>Increase Counter</Text>
-                </TouchableOpacity>
+                <TextInput
+                    onFocus={() => setItemFocused(true)}
+                    onBlur={() => setItemFocused(false)}
+                    placeholder="What do you want to store?"
+                    placeholderTextColor={theme.placeholderColor.val}
+                    style={[
+                        styles.textInput,
+                        isItemFocused ? styles.textInputFocus : styles.textInputBlur
+                    ]} />
+                <View style={styles.addAnImageQuestion}>
+                    <Text style={styles.text}>Want to add an image?</Text>
+                    <View style={styles.imageSelection}>
+                        <TouchableOpacity style={styles.cameraSelection}>
+                            <IconSymbol name="camera" color={theme.accentColor.val} />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.gallerySelection}>
+                            <IconSymbol name="image" color={theme.accentColor.val} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         </CustomSafeAreaView>
     );
@@ -50,26 +49,60 @@ export default function AddPage() {
 const stylesheet = (
     color?: string,
     accentColor?: string,
-    backgroundColor?: string
-) => {
-    console.log('Styles rendered!');
-
-    return StyleSheet.create({
-        screen: {
-            backgroundColor: backgroundColor
-        },
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
-        button: {
-            paddingVertical: 10,
-            marginVertical: 10,
-            backgroundColor: accentColor
-        },
-        text: {
-            color: color
-        }
-    });
-}
+    backgroundColor?: string,
+    borderColor?: string,
+    borderColorFocus?: string,
+    shadowColor?: string
+) => StyleSheet.create({
+    screen: {
+        backgroundColor: backgroundColor
+    },
+    container: {
+        flex: 1,
+        marginHorizontal: 10,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    button: {
+        paddingVertical: 10,
+        marginVertical: 10,
+        backgroundColor: accentColor
+    },
+    text: {
+        color: color,
+        paddingLeft: 10
+    },
+    textInput: {
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        width: '100%',
+        shadowColor: shadowColor,
+        borderColor: borderColor,
+        marginVertical: 10
+    },
+    textInputFocus: {
+        borderWidth: 2,
+        borderColor: borderColorFocus
+    },
+    textInputBlur: {
+        borderWidth: 1,
+        borderColor: borderColor,
+    },
+    addAnImageQuestion: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10
+    },
+    imageSelection: {
+        flexDirection: 'row'
+    },
+    cameraSelection: {
+        paddingRight: 10
+    },
+    gallerySelection: {
+        paddingLeft: 10,
+        paddingRight: 5
+    }
+});
